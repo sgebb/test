d10df30e792139a1e57edc74ddf1735342c08c6c
= <snorre.stenerud@gmail.com>
2017-05-14 16:16:26 +0200

    changed the post

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -25,8 +25,9 @@ namespace test.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public string Post([FromBody]string value)
         {
+            return ("I accept your json");
         }
 
         // PUT api/values/5
